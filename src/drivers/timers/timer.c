fc60a27112bacdbdaa78a93ad694078d09dd695a
rodrigo-olaya <rom.fitz@gmail.com>
2026-06-27 18:15:24 -0400

    finish C implementation of timers

--- src/drivers/timers/timer.c
+++ src/drivers/timers/timer.c
@@ -3,4 +3,10 @@
 void initTimers() {
     pRCC->APB1ENR |= (0x01 << TIM2_EN);
     *NVIC_ISER0 |= (0x01 << NVIC_TIM2_POSITION);
+
+    pTIM2->ARR = TIM2_ARR;
+    pTIM2->PSC = TIM2_PSC;
+
+    pTIM2->DIER |= (0x01 << TIM2_UIE);
+    pTIM2->CR1 |= (0x01 << TIM2_CEN);
 }
--- src/drivers/timers/timer.h
+++ src/drivers/timers/timer.h
@@ -4,11 +4,42 @@
 #include <stdint.h>
 #include "../registers.h"
 
+typedef struct {
+    volatile uint32_t CR1;
+    volatile uint32_t CR2;
+    volatile uint32_t SMCR;
+    volatile uint32_t DIER;
+    volatile uint32_t SR;
+    volatile uint32_t EGR;
+    volatile uint32_t CCMR1;
+    volatile uint32_t CCMR2;
+    volatile uint32_t CCER;
+    volatile uint32_t CNT;
+    volatile uint32_t PSC;
+    volatile uint32_t ARR;
+    volatile uint32_t RCR;
+    volatile uint32_t CCR1;
+    volatile uint32_t CCR2;
+    volatile uint32_t CCR3;
+    volatile uint32_t CCR4;
+    volatile uint32_t BDTR;
+    volatile uint32_t DCR;
+    volatile uint32_t DMAR;
+    volatile uint32_t TIM2_OR;
+} TIM_RegDef_t;
+
 #define pRCC ((RCC_RegDef_t*)0x40023800)
 #define NVIC_ISER0 ((volatile uint32_t*)0xE000E100)
 
 #define NVIC_TIM2_POSITION 28
 #define TIM2_EN 0
+
+#define pTIM2 ((TIM_RegDef_t*)0x40000000)
+
+#define TIM2_ARR 1000
+#define TIM2_PSC 15
+#define TIM2_UIE 0
+#define TIM2_CEN 0
 
 void initTimers();
 
