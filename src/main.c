6c04299b428f4b4269a45ede4620e1597e3de67f
rodrigo-olaya <rom.fitz@gmail.com>
2026-08-02 18:23:30 -0400

    RX working on logic analyzer

--- src/main.c
+++ src/main.c
@@ -10,12 +10,18 @@ void main() {
         // uartSendByte('B');
         // uint32_t start = tick;
         // while (tick - start < 1000);
-        uartTxEnqueue(0xAA);
-        uartTxEnqueue(0x01);
-        uartTxEnqueue('H');
-        uartTxEnqueue('B');
-        uartTxEnqueue('H');
-        uartTxEnqueue('B');
+
+        int16_t received;
+        while ((received = uartRxDequeue()) != -1) {
+            uartTxEnqueue((uint8_t)received);
+        }
+
+        // uartTxEnqueue(0xAA);
+        // uartTxEnqueue(0x01);
+        // uartTxEnqueue('H');
+        // uartTxEnqueue('B');
+        // uartTxEnqueue('H');
+        // uartTxEnqueue('B');
         uint32_t start = tick;
         while (tick - start < 1000);
     }
